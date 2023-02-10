306e451a84bd2311da22f24ba0df3df60647d2f5
Piper McCorkle <contact@piperswe.me>
2023-02-09 21:54:05 -0600

    Fix some overflow issues

--- src/lib/CodeBlock.svelte
+++ src/lib/CodeBlock.svelte
@@ -10,7 +10,8 @@
 <pre><code>{@html Prism.highlight(code, Prism.languages[lang], lang)}</code></pre>
 
 <style>
-	code {
+	pre {
+		overflow-x: auto;
 		font-family: var(--sc-mono-font-family);
 	}
 </style>
--- src/lib/Container.svelte
+++ src/lib/Container.svelte
@@ -2,10 +2,11 @@
 
 <style>
 	.sc-container {
-		width: 100%;
+		--sc-container-padding: 1.5rem;
+		width: calc(100% - var(--sc-container-padding));
 		max-width: 1000px;
-		padding-right: calc(1.5rem / 2);
-		padding-left: calc(1.5rem / 2);
+		padding-right: calc(var(--sc-container-padding) / 2);
+		padding-left: calc(var(--sc-container-padding) / 2);
 		margin-right: auto;
 		margin-left: auto;
 	}
