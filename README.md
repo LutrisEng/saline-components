2f721a15990e5a21ca0c7b56c5e7c03a4c0f5c06
Piper McCorkle <contact@piperswe.me>
2023-02-09 22:56:15 -0600

    Make theme a component

--- README.md
+++ README.md
@@ -7,10 +7,12 @@ In your Svelte files:
 ```svelte
 <script lang="ts">
 	// Import a theme in your layout
-	import '@lutris/saline-components/themes/lutris';
+	import LutrisTheme from '@lutris/saline-components/themes/lutris.svelte';
 	// Import components on your pages
 	import { Container, Heading, ProjectBadge } from '@lutris/saline-components';
 </script>
+
+<LutrisTheme />
 
 <Container>
 	<Heading level={1}>My Page</Heading>
--- src/routes/example.ts
+++ src/routes/example.ts
@@ -1,9 +1,11 @@
 export default `<script lang="ts">
     // Import a theme in your layout
-    import '@lutris/saline-components/themes/lutris';
+    import LutrisTheme from '@lutris/saline-components/themes/lutris.svelte';
     // Import components on your pages
     import { Container, Heading, ProjectBadge } from '@lutris/saline-components';
 </script>
+
+<LutrisTheme />
 
 <Container>
     <Heading level={1}>My Page</Heading>
